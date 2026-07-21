4a4be02bdb74645906ef645b6e62068663458783
rktrakesh <rtripathy324@gmail.com>
2026-07-21 12:01:01 +0530

    Remove target folder from tracking

--- target/classes/db/migration/V23__insert_default_cashbook_accounts.sql
+++
@@ -1,5 +0,0 @@
--- V23__insert_default_cashbook_accounts.sql
-INSERT INTO cashbook_accounts (account_name, account_type, opening_balance, financial_year, status, created_by,
-                               created_date)
-VALUES ('Main Cash', 'CASH', 0.00, '2024-25', 'ACTIVE', 'SYSTEM', NOW()),
-       ('Main Bank Account', 'BANK', 0.00, '2024-25', 'ACTIVE', 'SYSTEM', NOW());
